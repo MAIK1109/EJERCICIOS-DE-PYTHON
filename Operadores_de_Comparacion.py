c597b201c28ee878a61daaedc891c9b87c16f66d
Maycol Saavedra Saiz <maycolsa@gmail.com>
2025-07-23 20:13:10 -0500

    Ejercio de  condicionales if, elif, else

---
+++ Operadores_de_Comparacion.py
@@ -0,0 +1,10 @@
+entrada = str(input("Ingresa Una Entrada : "))
+
+if entrada == "Espatifilo" :
+    print("¡Espatifilo es la mejor planta de todas!")
+elif entrada == "ESPATIFILO":
+    print("Si - ¡El Espatifilo! es la mejor planta de todos los tiempos!" )
+elif entrada == "espatifilo":
+    print("No, ¡quiero un gran Espatifilo!") 
+else:
+    print("¡Espatifilo!, ¡No [",entrada,"]!")

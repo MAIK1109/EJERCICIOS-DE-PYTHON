96dc5c9eb6fec71b0a536f5258083d9d5d8310e5
Maycol Saavedra Saiz <maycolsa@gmail.com>
2025-07-24 19:30:11 -0500

    Bucle utilizando Break

---
+++ Bucle_Break.py
@@ -0,0 +1,10 @@
+palabra = str(input("INGRESA UNA PALABRA : "))
+
+while True:
+    if palabra =="Chupacabra":
+        print("Chupacabra")
+        break
+    else:
+        palabra = str(input("INGRESA OTRA PALABRA: "))
+
+
